4df4cb73c9fcddaa8ed566660945654ccf083f95
orrollo <orrollo@gmail.com>
2020-11-12 19:32:27 +0300

    поменял main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,19 @@ import model.triangle.Triangle;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle[] triangles = new  Triangle[4];
-        triangles[0] = new Triangle(4,5,6);
-        triangles[1] = new IsoscelesTriangle(5,4);
-        triangles[2] = new EquilateralTriangle(3);
-        triangles[3] = new RectangularTriangle(3, 4);
+
+        Triangle[] triangles = new Triangle[] {
+            new Triangle(4,5,6),
+            new IsoscelesTriangle(5,4),
+            new EquilateralTriangle(3),
+            new RectangularTriangle(3, 4)
+        };
+
+//        Triangle[] triangles = new  Triangle[4];
+//        triangles[0] = new Triangle(4,5,6);
+//        triangles[1] = new IsoscelesTriangle(5,4);
+//        triangles[2] = new EquilateralTriangle(3);
+//        triangles[3] = new RectangularTriangle(3, 4);
         for (Triangle triangle : triangles) {
             System.out.println(triangle);
         }
